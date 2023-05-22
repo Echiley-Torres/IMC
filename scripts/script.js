88c7d918bc79a3b4cd4abdb26b5ea27ece4be04c
Échiley Torres <130602775+Echiley-Torres@users.noreply.github.com>
2023-05-22 16:41:39 -0300

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -10,17 +10,26 @@ function calcular(){
 
     var imc = peso/(alt*alt)
 
+    var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
     if(imc <= 16.9){
-        res.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> Você está muito abaixo do peso`
+        res.innerHTML = `<p>Seu IMC é <strong>${imc.toFixed(2)}</strong> Você está muito abaixo do peso</p>`
+        img.setAttribute('src', 'img-1-map.png')
     } else if(imc <= 18.4 &&  imc >= 17){
-        res.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> Você está abaixo do peso`
+        res.innerHTML = `<p>Seu IMC é <strong>${imc.toFixed(2)}</strong> Você está abaixo do peso</p>`
+        img.setAttribute('src', 'img-abaixopeso.png')
     } else if( imc >= 18.6 && imc <= 24.9){
-        res.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> Peso ideal`
+        res.innerHTML = `<p>Seu IMC é <strong>${imc.toFixed(2)}</strong> Peso ideal</p>`
+        img.setAttribute('src', 'img-pesoideal.png')
     } else if (imc >= 25 && imc <= 29.9 ){
-        res.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> Acima do peso`
+        res.innerHTML = `<p>Seu IMC é <strong>${imc.toFixed(2)}</strong> Acima do peso</p>`
+        img.setAttribute('src', 'img-acimapeso.png')
     } else if(imc >= 30){
-        res.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> Você está com Obesidade`
+        res.innerHTML = `<p>Seu IMC é <strong>${imc.toFixed(2)}</strong> Você está com Obesidade</ps>`
+        img.setAttribute('src', 'img-obesidade.png')
     }
-        
+    res.style.textAlign = 'center'
+    res.appendChild(img)
 
 }
